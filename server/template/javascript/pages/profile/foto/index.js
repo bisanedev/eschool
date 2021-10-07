af15bd3c8b969deaf56f939bc1ce472cdc1ae71a
bisanedev <zvickyhac@gmail.com>
2021-10-08 05:42:24 +0700

    update

--- server/template/javascript/pages/profile/foto/index.js
+++ server/template/javascript/pages/profile/foto/index.js
@@ -10,11 +10,13 @@ class PageProfileFoto extends React.Component{
     super(props);
     this.state = {
       src: null,
+      croppedImageUrl:"",
+      errorSelect:"",
       crop: {
         unit: 'px',        
         aspect: 3 / 4,
-        with:108,
-        height:128
+        width:260,
+        height:320
       }
     }    
   }
@@ -25,7 +27,7 @@ class PageProfileFoto extends React.Component{
 
   render() {
     let foto = <img src={"data/users/"+this.props.tokenData.username+".jpg"} onError={(e)=>{e.target.onerror = null; e.target.src=this.props.tokenData.jenis==="pria" ? "assets/images/cowok.png":"assets/images/cewek.png"}} />;
-    const { crop, croppedImageUrl, src } = this.state;
+    const { crop, croppedImageUrl, src ,errorSelect } = this.state;
     return (  
     <div className="konten"> 
         <Helmet>
@@ -46,19 +48,18 @@ class PageProfileFoto extends React.Component{
               <div className="row">
               <div className="col-md-9">                
                 <input className="form-control mb-3" type="file" accept="image/*" onChange={this.onSelectFile}/>
-                {src && (
+                {src != null ? (
                   <ReactCrop
                     src={src}
                     crop={crop}
                     ruleOfThirds
-                    minHeight={108}
-                    minWidth={128} 
-                    zoom                                                
+                    minHeight={320}
+                    minWidth={260}                                                                 
                     onImageLoaded={this.onImageLoaded}
                     onComplete={this.onCropComplete}
                     onChange={this.onCropChange}
                   />
-                )}
+                ):(<h5 className="p-5" style={{display:"flex",alignItems:"center",justifyContent:"center"}}>{errorSelect}</h5>)}
               </div> 
               <div className="col-md-3" style={{display:"flex",flexDirection:"column"}}>
               <button type="button" className="btn btn-primary mb-3" disabled={croppedImageUrl ? false:true}>Upload</button>
@@ -75,14 +76,29 @@ class PageProfileFoto extends React.Component{
   }
   // ---------------------------- script 
   onSelectFile = (e) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const reader = new FileReader();
-      reader.addEventListener('load', () =>
-        this.setState({ src: reader.result })
-      );
-      reader.readAsDataURL(e.target.files[0]);
+    let wow = this; 
+    if (e.target.files && e.target.files.length > 0) {      
+      const reader = new FileReader();   
+      reader.addEventListener('load', () => {        
+        var image = new Image();        
+        image.src = reader.result;   
+        image.addEventListener('load', function () {
+          var height = this.height;
+          var width = this.width;
+          console.log(`height: ${this.height}, width: ${this.width}`);          
+          if (height >= 320 && width >= 260) {
+            console.log("berhasil");
+            wow.setState({ src: reader.result });                                    
+          }else{
+            console.log("gagal");
+            wow.setState({ src: null,croppedImageUrl:"",errorSelect:"Gambar foto resolusi minimal 320x260" }); 
+          }
+        });        
+      });
+      reader.readAsDataURL(e.target.files[0]);   
     }
   };
+
   onImageLoaded = (image) => {
     this.imageRef = image;
   };

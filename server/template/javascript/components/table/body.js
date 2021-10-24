05c1e5ead42a879fff3bd2a913ca4709b20d6d9b
bisanedev <zvickyhac@gmail.com>
2021-10-24 19:47:46 +0700

    update

--- server/template/javascript/components/table/body.js
+++ server/template/javascript/components/table/body.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Body(props) {
     return (
-        <div className="flex flex-wrap pa3">
+        <div className="flex flex-wrap pa3" style={{minHeight:"310px"}}>
           {props.children}           
         </div>
     );

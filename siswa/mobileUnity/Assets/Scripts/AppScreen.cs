0ef4876fd9ba4161f41abf4cc30939b0cd12dd01
bisanedev <zvickyhac@gmail.com>
2021-11-08 12:57:33 +0700

    update

--- siswa/mobileUnity/Assets/Scripts/AppScreen.cs
+++ siswa/mobileUnity/Assets/Scripts/AppScreen.cs
@@ -7,8 +7,11 @@ using UnityEngine.Networking;
 
 public class AppScreen : MonoBehaviour
 {     
-    public ApiNetworkManager ApiManager;  
-
+    public ApiNetworkManager ApiManager;
+    public Button LogoutTombol;  
+    public Button GantiPasswordTombol;
+    public Text NamaSiswa;
+    public Text KelasNama;    
     void Start()
     {
         //autorotationPortrait
@@ -19,10 +22,18 @@ public class AppScreen : MonoBehaviour
         Screen.orientation = ScreenOrientation.AutoRotation;
         //jika tidak ada userToken maka login
         if (!PlayerPrefs.HasKey("userToken")){ SceneManager.LoadScene("LoginScreen", LoadSceneMode.Single);}
-        // intialisasi tombols                             
+        // intialisasi tombols  
+        GantiPasswordTombol.onClick.AddListener(gantiPassword);                           
+        LogoutTombol.onClick.AddListener(Logout);
+        User dataUser = JsonUtility.FromJson<User>(PlayerPrefs.GetString("userData")); 
+        NamaSiswa.text = dataUser.username;
+        KelasNama.text = dataUser.jenis;                
     }
 
- 
+    void gantiPassword()
+    {
+      Debug.Log("Ganti Tombol");
+    }
     void Logout()
     {
       PlayerPrefs.DeleteAll();

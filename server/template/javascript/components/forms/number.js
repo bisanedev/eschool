50389792c7077ce162406a7ffebb6ab4b7a177c0
bisanedev <zvickyhac@gmail.com>
2021-12-12 15:07:07 +0700

    update

--- server/template/javascript/components/forms/number.js
+++ server/template/javascript/components/forms/number.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function InputNumber(props) {
     return (
-        <input type="number" min={props.min} max={props.max} name={props.name} value={props.value} placeholder={props.placeholder} className="input-reset ba b--black-20 pa2 db w-100" onChange={props.onChange}/>           
+        <input type="text" name={props.name} pattern="[0-9]*" value={props.value} placeholder={props.placeholder} className="input-reset ba b--black-20 pa2 db w-100" onChange={props.onChange}/>           
     );
 }
 
--- server/template/javascript/pages/aplikasi/quiz/paket/add.js
+++ server/template/javascript/pages/aplikasi/quiz/paket/add.js
@@ -1,9 +1,8 @@
 import React from "react";
 import axios from "axios";
-import Cropper from "react-cropper";
 import { Helmet } from "react-helmet";
 import { Breadcrumb } from "../../../../components/menu";
-import { Cards } from "../../../../components/forms";
+import { Cards,InputText,InputNumber } from "../../../../components/forms";
 import { ToastContainer, toast } from 'react-toastify';
 
 class PageAplikasiQuizPaketSoalAdd extends React.Component{
@@ -12,6 +11,8 @@ class PageAplikasiQuizPaketSoalAdd extends React.Component{
       super(props);
       this.state = {
           data:[],
+          bobotPilihan:0,
+          bobotEssay:0,           
           isLoading:true,
       } 
 
@@ -32,8 +33,9 @@ class PageAplikasiQuizPaketSoalAdd extends React.Component{
 
     
   render() {     
-    const {uploadProgress,uploadDisable,tingkatan,mapel,semester} = this.state; 
-    const uploadClass = uploadProgress ? "progress-active":"";     
+    const {uploadProgress,uploadDisable,tingkatan,mapel,semester,bobotPilihan,bobotEssay} = this.state; 
+    const uploadClass = uploadProgress ? "progress-active":""; 
+    const totalBobot = parseInt(bobotPilihan)+parseInt(bobotEssay);    
     return (  
     <div className="konten"> 
         <Helmet>
@@ -52,7 +54,34 @@ class PageAplikasiQuizPaketSoalAdd extends React.Component{
           </Breadcrumb>   
         </div>
         <div className="mw9 center cf ph3 flex">
-
+        <div className="w-100">
+        <Cards title="Menambahkan paket soal" bodyClass="flex">
+        <div className="w-50 pa3">
+          <div className="w-100 mb3">
+            <label className="f5 fw4 db mb2">Nama paket soal</label>
+            <InputText name="nama" placeholder="ketik nama paket soal disini" onChange={this.handleInputChange} />
+          </div>
+        </div>
+        <div className="w-50 pa3">
+          <div className="w-100 mb3">
+            <label className="f5 fw4 db mb2">Bobot nilai (total bobot nilai pilihan ganda + essay = wajib 100)</label>
+            <div className="flex">
+            <div className="w-30">
+              <InputNumber name="bobotPilihan" value={bobotPilihan} placeholder="pilihan ganda" onChange={this.handleChangePilihan} />
+            </div>
+            <div className="w-10 f3 flex items-center justify-center">+</div>
+            <div className="w-30">
+              <InputNumber name="bobotEssay" value={bobotEssay} placeholder="essay" onChange={this.handleChangeEssay} />
+            </div> 
+            <div className="w-10 f3 flex items-center justify-center">=</div>
+            <div className="w-20">
+              <InputText value={isNaN(totalBobot) ? 0:totalBobot } readonly={true} />
+            </div>
+            </div>      
+          </div>
+        </div>
+        </Cards>
+        </div>
         </div>
         <ToastContainer />
     </div>
@@ -65,6 +94,29 @@ class PageAplikasiQuizPaketSoalAdd extends React.Component{
     const name = target.name;
     this.setState({ [name]: value});    
   }
+
+  handleChangePilihan = (event) => { 
+    const {bobotPilihan} = this.state;    
+    const target = event.target;     
+    const value = target.validity.valid ? target.value : bobotPilihan;    
+    const name = target.name;    
+    const numericRegex = /^(0|[1-9][0-9]?|100)$/;
+    if(numericRegex.test(value) || value === "") {
+      this.setState({ [name]: value });
+    }     
+  }
+
+  handleChangeEssay = (event) => {
+    const {bobotEssay} = this.state;    
+    const target = event.target;    
+    const value = target.validity.valid ? target.value : bobotEssay;    
+    const name = target.name;
+    const numericRegex = /^(0|[1-9][0-9]?|100)$/;
+    if(numericRegex.test(value) || value === "") {
+      this.setState({ [name]: value });
+    }      
+  }
+
   /*--- fetch data ---*/
    fetchData = (tingkat,mapel,semester) => {   
     axios.get(

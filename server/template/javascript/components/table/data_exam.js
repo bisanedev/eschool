27742f2f9a5344fd15a1a7454a2df1baf7b26647
bisanedev <zvickyhac@gmail.com>
2022-02-03 11:31:50 +0700

    update

--- server/template/javascript/components/table/data_exam.js
+++ server/template/javascript/components/table/data_exam.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from "moment";
+import "moment/locale/id";
 
 function DataExam(props) {     
     let foto = <img src={props.src} onError={(e)=>{e.target.onerror = null; e.target.src=props.data.jenis==="l" ? "assets/images/cowok.png":"assets/images/cewek.png"}} />;
@@ -16,7 +17,7 @@ function DataExam(props) {
                 <div className="title flex items-center justify-center pa1">
                     <span className="elipsis">{props.nama}</span>
                 </div>
-                <div className="body flex flex-column justify-center items-center pa1">
+                <div className="body flex flex-column justify-center items-center ph1">
                     <div className="mb1 w-100">
                         <div className="flex justify-between title-waktu">
                             <span>Mulai</span>
@@ -24,19 +25,26 @@ function DataExam(props) {
                         </div>
                         <span className="tgl">{moment(props.data.mulai).format('dddd')}, {moment(props.data.mulai).format('DD/MM/YYYY')}</span>
                     </div>
-                    <div className="w-100">
+                    <div className="mb1 w-100">
                         <div className="flex justify-between title-waktu">
                             <span>Selesai</span>
                             <span className="jam bg-red ph1">{moment(props.data.selesai).format('HH:mm')}</span>
                         </div>
                         <span className="tgl">{moment(props.data.selesai).format('dddd')}, {moment(props.data.selesai).format('DD/MM/YYYY')}</span>
                     </div>
+                    <div className="w-100">
+                        <div className="flex w-100 justify-between title-waktu">
+                            <span>KKM</span>
+                            <span className="ph1 bg-gold">{props.data.nilai_minimal}</span>
+                        </div>
+                        <span className="db b" style={{fontSize:12}}>{props.data.paket_soal.length} paket soal</span>
+                    </div>
                 </div>  
                 <div className="footer">
-                    <a className="flex ph2 link dim pointer primary" style={{fontSize:11}} href={props.onLihat}>
+                    <a className="flex pa2 link dim pointer primary" style={{fontSize:11}} href={props.onLihat}>
                         <i className="material-icons" style={{fontSize:"11px"}}>leaderboard</i>&nbsp; Lihat nilai
                     </a>
-                    <a className="flex ph2 link dim pointer primary" style={{fontSize:11}} href={props.onEdit}>
+                    <a className="flex pa2 link dim pointer primary" style={{fontSize:11}} href={props.onEdit}>
                         <i className="material-icons" style={{fontSize:"11px"}}>edit</i>&nbsp; Ubah
                     </a>
                 </div>  
